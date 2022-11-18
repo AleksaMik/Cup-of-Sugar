f159592d6030627ac414aca6c79a0316a58ea5c0
AleksaMik <aleksam.2509@gmail.com>
2022-11-18 15:27:11 -0600

    pageLoad

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 // import Category from "../components/Category";
 // import Cart from "../components/Cart";
+import LoginForm from "../pages/LoginForm";
 
 const Home = () => {
   return (
     <div className="container">
-     <Navbar/>
+     <LoginForm/>
     </div>
   );
 };
--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,31 +1,31 @@
-export const getSavedRentalIds = () => {
-    const savedRentalIds = localStorage.getItem('saved_rentals')
-      ? JSON.parse(localStorage.getItem('saved_rentals'))
-      : [];
+// export const getSavedRentalIds = () => {
+//     const savedRentalIds = localStorage.getItem('saved_rentals')
+//       ? JSON.parse(localStorage.getItem('saved_rentals'))
+//       : [];
   
-    return savedRentalIds;
-  };
+//     return savedRentalIds;
+//   };
   
-  export const saveRentalIds = (rentalIdArr) => {
-    if (rentalIdArr.length) {
-      localStorage.setItem('saved_rentals', JSON.stringify(rentalIdArr));
-    } else {
-      localStorage.removeItem('saved_rentals');
-    }
-  };
+//   export const saveRentalIds = (rentalIdArr) => {
+//     if (rentalIdArr.length) {
+//       localStorage.setItem('saved_rentals', JSON.stringify(rentalIdArr));
+//     } else {
+//       localStorage.removeItem('saved_rentals');
+//     }
+//   };
   
-  export const removeRentalId = (rentalId) => {
-    const savedRentalIds = localStorage.getItem('saved_rentals')
-      ? JSON.parse(localStorage.getItem('saved_rentals'))
-      : null;
+//   export const removeRentalId = (rentalId) => {
+//     const savedRentalIds = localStorage.getItem('saved_rentals')
+//       ? JSON.parse(localStorage.getItem('saved_rentals'))
+//       : null;
   
-    if (!savedRentalsIds) {
-      return false;
-    }
+//     if (!savedRentalsIds) {
+//       return false;
+//     }
   
-    const updatedSavedRentalIds = savedRentalIds?.filter((savedRentalId) => savedRentalId !== rentalId);
-    localStorage.setItem('saved_rental', JSON.stringify(updatedSavedRentalIds));
+//     const updatedSavedRentalIds = savedRentalIds?.filter((savedRentalId) => savedRentalId !== rentalId);
+//     localStorage.setItem('saved_rental', JSON.stringify(updatedSavedRentalIds));
   
-    return true;
-  };
+//     return true;
+//   };
   
