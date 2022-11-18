992c9791d7c26e9f7167064617acbc181200ddd6
kyky19 <kyky19@me.com>
2022-11-17 20:58:03 -0600

    page is loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Home from './pages/Home';
-import Search from './pages/SearchRentals';
-import Saved from './pages/SavedRentals';
+import Home from "./pages/Home";
+import Search from "./pages/SearchRentals";
+import Saved from "./pages/SavedRentals";
 // import Post from './pages/PostRentals';
 // import { StoreProvider } from './utils/GlobalState';
 
-
 const httpLink = createHttpLink({
-    uri: '/graphql',
+  uri: "/graphql",
 });
 
-const authLink = setContext((_, {headers}) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <Routes>
-                        <Route
-                        path='/' element={<Home />}/>
-                        <Route
-                        path='/search' element={<Search />}/>
-                        <Route
-                        path='/saved' element={<Saved />}/>
-                        <Route
-                        path='/post' element={<Post />}/>
-                    </Routes>
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/saved" element={<Saved />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
-export default App; 
+export default App;
