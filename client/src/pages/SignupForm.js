a07e3372da6f9c12e01e28e3f2db5881c737e7da
kyky19 <kyky19@me.com>
2022-11-20 16:35:05 -0600

    updated signupform to include firstname lastname as seen in mutations.

--- client/src/pages/SignupForm.js
+++ client/src/pages/SignupForm.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
@@ -85,4 +90,3 @@ function Signup(props) {
 }
 
 export default Signup;
-
