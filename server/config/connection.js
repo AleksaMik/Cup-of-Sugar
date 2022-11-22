402cc9c2217e3fdf6428fb607addc6afe0b43539
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-11-22 17:29:28 -0600

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/cup-of-sugar",
+  process.env.MONGODB_URI || "mongodb+srv://unicorn:wordpass@cluster0.ooujm2n.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
